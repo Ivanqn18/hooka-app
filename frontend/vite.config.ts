d40d8462b261418fc1faf91177bb7eea3e72637a
Raúl Alonso <byrushcoc@gmail.com>
2026-05-20 10:41:21 +0200

    cambios

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     react(),
     compression({
-      algorithms: "brotliCompress",
+      algorithms: ["brotliCompress"],
       exclude: [/\.(br)$/, /\.(gz)$/, /\.html$/],
     }),
     visualizer({
